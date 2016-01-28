fdbbd01f5dacab379660cae6326ba1650bc249ef
Arturo Raygoza <arturoraygozajr@gmail.com>
2016-01-27 18:36:31 -0800

    impliment put

--- routes/items.js
+++ routes/items.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Item = require('../models/Item');
 
+// Create
 router.post('/', function(req, res, next){
   console.log("got body ", req.body)
   var newItem = new Item(req.body);
   console.log("new mongoose item", newItem)
-	newItem.save(function(err, savedItem){
-		if(err) return res.send(err);
+  newItem.save(function(err, savedItem){
+    if(err) return res.send(err);
     res.send(savedItem);
-	})
+  })
 })
 
+// Read
 router.get('/', function(req, res){
 	Item.find({}, function(err, foundItems){
     if(err) return res.send(err);
@@ -19,12 +21,27 @@ router.get('/', function(req, res){
 	})
 })
 
+
+// Update
+router.put('/', function(req, res){
+  Item.findByIdAndUpdate(req.body._id, req.body, function(err, doc){
+    if(err) return res.send(err);
+    res.send(req.body);
+  });
+})
+
+
+// Delete
 router.delete('/:id', function(req, res){
   Item.findByIdAndRemove(req.params.id, function(err){
     if(err) return res.send(err);
     res.send("Deleted!");
   })
 })
+
+
+
+
 
 
 module.exports = router;
